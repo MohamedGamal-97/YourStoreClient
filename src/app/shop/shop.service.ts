87ccfca19c263ceb1b61958ce12c08a91542bbfd
mohamed gamal <midogemy100@gmail.com>
2022-05-05 19:42:43 +0200

    structure files and folders

---
+++ src/app/shop/shop.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl= 'https://localhost:7260/api/';
+
+  constructor(private http:HttpClient) { }
+  getProducts(){
+    return this.http.get<IPagination>(this.baseUrl+'products?pageSize=100');
+  }
+}
