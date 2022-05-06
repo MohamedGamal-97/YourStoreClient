05e9219b0ba7ab819369f1885ebb913dfb6679f0
mohamed gamal <midogemy100@gmail.com>
2022-05-06 08:20:18 +0200

    update routing

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
+import { IProduct } from '../shared/models/product';
 import { IType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/ShopParams';
 
@@ -51,4 +52,7 @@ export class ShopService {
     return this.http.get<IType[]>(this.baseUrl+'products/types');
   }
 
+  getProduct(id:number){
+    return this.http.get<IProduct>(this.baseUrl+'products/'+id);
+  }
 }
